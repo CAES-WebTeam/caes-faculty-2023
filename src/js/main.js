b278fa00a3b8f40ae75a912c44f6cdba78528340
“Ashley <awilliams@uga.edu>
2022-11-30 20:56:24 -0500

    Friendship ended with Parvus, now Tobii is my now my lightbox js library

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-/** PARVUS - Parvus is a lightbox library for images **/
+// /** PARVUS - Parvus is a lightbox library for images **/
+
+// // First, find all the image blocks that link to an image.
+// const linkedImgs = document.querySelectorAll('.wp-block-image a[href*=".jpg"],.wp-block-image a[href*=".jpeg"],.wp-block-image a[href*=".png"],.wp-block-image a[href*=".gif"]');
+// for (const link of linkedImgs) {
+//    link.classList.add('lightbox');
+//   }
+
+// // Import Parvus
+// import Parvus from 'parvus';
+
+// // Initialize Parvus
+// const prvs = new Parvus({
+//     gallerySelector: '.wp-block-gallery',
+//     captionsSelector: 'img',
+//     captionsAttribute: 'alt'
+// })
+
+// Import Tobii
+import Tobii from '@midzer/tobii';
 
 // First, find all the image blocks that link to an image.
 const linkedImgs = document.querySelectorAll('.wp-block-image a[href*=".jpg"],.wp-block-image a[href*=".jpeg"],.wp-block-image a[href*=".png"],.wp-block-image a[href*=".gif"]');
@@ -6,12 +25,16 @@ for (const link of linkedImgs) {
    link.classList.add('lightbox');
   }
 
-// Import Parvus
-import Parvus from 'parvus';
+// Then, set data-group attribute on child images of each wp-block gallery.
+const wpGalleries = document.querySelectorAll('.wp-block-gallery');
 
-// Initialize Parvus
-const prvs = new Parvus({
-    gallerySelector: '.wp-block-gallery',
-    captionsSelector: 'img',
-    captionsAttribute: 'alt'
-})
+function setGroupAttr(gallery, index) {
+  const galleryImgs = gallery.querySelectorAll('.wp-block-image a[href*=".jpg"],.wp-block-image a[href*=".jpeg"],.wp-block-image a[href*=".png"],.wp-block-image a[href*=".gif"]');
+  for (const galleryImg of galleryImgs)  {
+    galleryImg.setAttribute("data-group", "gallery-" + index);
+  }
+}
+
+wpGalleries.forEach((gallery, index) => setGroupAttr(gallery, index));
+
+const tobii = new Tobii();
