fdd9fbc24733e27e0080ac4d7a91dca2743d425d
“Ashley <awilliams@uga.edu>
2022-12-13 09:03:52 -0500

    Minor adjustments

--- src/js/main.js
+++ src/js/main.js
@@ -11,30 +11,7 @@ import Parvus from 'parvus';
 
 // // Initialize Parvus
 const prvs = new Parvus({
-    // gallerySelector: '.wp-block-gallery',
+    gallerySelector: '.wp-block-gallery',
     captionsSelector: 'img',
     captionsAttribute: 'alt'
 })
-
-// // Import Tobii - commented out below because we're not using this library in general for the theme
-// import Tobii from '@midzer/tobii';
-
-// // First, find all the image blocks that link to an image.
-// const linkedImgs = document.querySelectorAll('.wp-block-image a[href*=".jpg"],.wp-block-image a[href*=".jpeg"],.wp-block-image a[href*=".png"],.wp-block-image a[href*=".gif"]');
-// for (const link of linkedImgs) {
-//    link.classList.add('lightbox');
-//   }
-
-// // Then, set data-group attribute on child images of each wp-block gallery.
-// const wpGalleries = document.querySelectorAll('.wp-block-gallery');
-
-// function setGroupAttr(gallery, index) {
-//   const galleryImgs = gallery.querySelectorAll('.wp-block-image a[href*=".jpg"],.wp-block-image a[href*=".jpeg"],.wp-block-image a[href*=".png"],.wp-block-image a[href*=".gif"]');
-//   for (const galleryImg of galleryImgs)  {
-//     galleryImg.setAttribute("data-group", "gallery-" + index);
-//   }
-// }
-
-// wpGalleries.forEach((gallery, index) => setGroupAttr(gallery, index));
-
-// const tobii = new Tobii();
