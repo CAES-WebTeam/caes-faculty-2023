6055bbb5a56fb5ce3b85f0292f0c7e79a1929918
“Ashley <awilliams@uga.edu>
2023-03-30 14:01:01 -0400

    Fixes for people profile patterns and single person page

--- patterns/people-cpt-3-list.php
+++ patterns/people-cpt-3-list.php
@@ -8,16 +8,16 @@
  * Viewport Width: 1100
  */
 ?>
-<!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"caes-people","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"lastname_az":"asc"},"displayLayout":{"type":"flex","columns":2},"namespace":"caes-people-profiles","className":"caes-people-profiles list"} -->
-<div class="wp-block-query caes-people-profiles list"><!-- wp:post-template -->
-    <!-- wp:columns {"className":"caes-people-profiles-group"} -->
-    <div class="wp-block-columns caes-people-profiles-group"><!-- wp:column {"width":"33.33%"} -->
-        <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:post-featured-image {"width":"150px","height":"150px","style":{"border":{"radius":"100px"}}} /--></div>
+<!-- wp:query {"query":{"perPage":"6","pages":0,"offset":0,"postType":"caes-people","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"firstorlast":"asc","lastname_az":"asc"},"displayLayout":{"type":"flex","columns":3},"namespace":"caes-people-list","className":"caes-people-profiles grid"} -->
+<div class="wp-block-query caes-people-profiles grid"><!-- wp:post-template -->
+    <!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+    <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:post-featured-image {"width":"150px","height":"150px","style":{"border":{"radius":"20rem"}},"className":"caes-people-profiles-img-container"} /--></div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group"><!-- wp:post-title {"textAlign":"left","isLink":true,"fontSize":"x-large"} /-->
 
                 <!-- wp:caes-people/position-title {"align":"left"} /-->
             </div>
